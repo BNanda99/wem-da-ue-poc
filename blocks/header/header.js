e182fa9587b3c6f046d36a956a5de2aa737f45e3
vn58fzy <vn58fzy@homeoffice.wal-mart.com>
2025-11-27 12:15:36 +0530

    page-break changes

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -174,6 +174,8 @@ async function buildBreadcrumbs() {
  */
 export default async function decorate(block) {
   // load nav as fragment
+
+  console.log(block);
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
   const fragment = await loadFragment(navPath);
